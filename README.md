723a8428b8c020abf45aa0a652480d0d345a4918
Jason R. McNeil <jason@jasonrm.net>
2016-08-25 22:56:55 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fargo
+
+Small Alpine-based docker image for use in moving files around.
+
+## Additional Tools
+
+* git
+* curl
+* [pixz](https://github.com/vasi/pixz)
+* [pv](http://www.ivarch.com/programs/pv.shtml)
+* [gof3r](https://github.com/rlmcpherson/s3gof3r)
---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+DEPS="libarchive git curl"
+BUILD_DEPS="asciidoc build-base libarchive-dev autoconf automake"
+apk add --update ${DEPS} ${BUILD_DEPS}
+
+# Build pixz
+git clone https://github.com/vasi/pixz.git \
+    && cd pixz \
+    && ./autogen.sh \
+    && ./configure \
+    && make \
+    && make install \
+    && cd .. \
+    && rm -Rf pixz
+
+# Build pv
+curl -L -O http://www.ivarch.com/programs/sources/pv-1.6.0.tar.gz \
+    && tar -xf pv-1.6.0.tar.gz \
+    && cd pv-1.6.0 \
+    && ./configure \
+    && make \
+    && make install \
+    && cd .. \
+    && rm -Rf pv-1.6.0
+
+# Install gof3r
+curl -L https://github.com/rlmcpherson/s3gof3r/releases/download/v0.5.0/gof3r_0.5.0_linux_amd64.tar.gz \
+    | gzip -d \
+    | tar --extract
+mv gof3r_0.5.0_linux_amd64/gof3r /usr/local/bin/gof3r
+rmdir gof3r_0.5.0_linux_amd64
+
+# Cleanup
+apk del ${BUILD_DEPS}
+rm -rf /var/cache/apk/*
+
+rm $0
